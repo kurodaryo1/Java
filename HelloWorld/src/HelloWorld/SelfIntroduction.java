db357b42d870d5dfa8e7399c3b502d54b38702a9
PC_User <PC_User@192.168.3.8>
2025-07-27 21:20:51 +0900

    Java3 導入

---
+++ HelloWorld/src/HelloWorld/SelfIntroduction.java
@@ -0,0 +1,27 @@
+package HelloWorld;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		//氏名
+		String name = "黒田遼";
+		
+		//年齢
+		int age = 21;
+		
+		//趣味
+		String hobby = "ゲーム";
+		
+		/*
+		 * 自己紹介を出力する処理
+		 */
+		
+		System.out.println("私の名前は" + name +"です。");
+		System.out.println("年齢は" + age +"です。");
+		System.out.println("趣味は" + hobby +"です。");
+		
+	}
+
+}
